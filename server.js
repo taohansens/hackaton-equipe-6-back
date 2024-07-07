19d1bbdf2298bbdb5f4d19067b8f436dbf90a4b9
taohansens <taohansens@gmail.com>
2024-07-07 17:22:32 -0300

    change port 9900

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/psychologists', psychologistRoutes);
 app.use('/api/appointments', appointmentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9900;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
